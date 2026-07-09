1ed6b021a762e458ebf4fd0205a638893dc26830
geopascual <christpascual@yahoo.com>
2026-07-09 17:10:37 +0800

    testing

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN mvn dependency:go-offline
 
 ENV TEST_GROUP=""
 
-CMD ["sh", "-c", "if [ -z \"$TEST_GROUP\" ]; then mvn test -Dheadless=true; else mvn test -Dheadless=true -Dgroups=$TEST_GROUP; fi"]
+CMD ["sh", "-c", "if [ -z \"$TEST_GROUP\" ]; then mvn test -Dheadless=true; else mvn test -Dheadless=true -Dgroups=${TEST_GROUP}; fi"]
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -9,28 +9,25 @@ public class BaseTest {
     protected Page page;
     protected ConfigReader config;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setup() {
 
-    System.out.println("========== SETUP START ==========");
+        System.out.println("========== SETUP START ==========");
 
-    config = new ConfigReader();
+        config = new ConfigReader();
 
-    boolean headless = Boolean.parseBoolean(
-        System.getProperty("headless", config.get("headless"))
-    );
+        boolean headless = Boolean.parseBoolean(
+            System.getProperty("headless", config.get("headless"))
+        );
 
-    page = PlaywrightFactory.initBrowser(headless);
+        page = PlaywrightFactory.initBrowser(headless);
 
-    System.out.println("===== BASE TEST SETUP END =====");
+        System.out.println("===== BASE TEST SETUP END =====");
+    }
 
-}
-
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void teardown() {
-
         PlaywrightFactory.tearDown();
-
     }
 
 }
